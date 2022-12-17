92a0208cdc510023595e3e84809288c20a117433
Raghav Mantri <42713275+raghavm1@users.noreply.github.com>
2022-12-17 16:17:01 +0800

    Update route for deployment on GP

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ export default {
   title: "Raghav Mantri",
   siteTitle: "Raghav Mantri",
   logo: "",
-  base: "/raghavm1.github.io/",
+  base: "",
   description: "About me (मेरे बारे मेँ)",
   themeConfig: {
     nav: [{ text: "Get in touch", link: "mailto:mantriraghav11@gmail.com" }],
