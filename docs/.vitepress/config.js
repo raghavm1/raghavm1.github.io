920f9ed4659aa8865fdcb473073aa2807c23a89d
Raghav Mantri <42713275+raghavm1@users.noreply.github.com>
2022-12-17 16:11:53 +0800

    Add deployment script

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ export default {
   title: "Raghav Mantri",
   siteTitle: "Raghav Mantri",
   logo: "",
+  base: "/raghavm1.github.io/",
   description: "About me (मेरे बारे मेँ)",
   themeConfig: {
     nav: [{ text: "Get in touch", link: "mailto:mantriraghav11@gmail.com" }],
